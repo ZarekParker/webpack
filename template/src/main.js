f0c685efb53df08c06c2c009dc063ab785ecbce9
mauro <maurop123@gmail.com>
2018-01-13 07:06:15 -0300

    update template with vuetify-specific changes

--- template/src/main.js
+++ template/src/main.js
@@ -7,10 +7,59 @@ import App from './App'
 {{#router}}
 import router from './router'
 {{/router}}
+{{#alacarte}}
+import {
+  Vuetify,
+  VApp,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar,
+  transitions
+} from 'vuetify'
+import '../node_modules/vuetify/src/stylus/app.styl'
+{{else}}
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+{{/alacarte}}
 
-Vue.use(Vuetify)
+{{#alacarte}}
+Vue.use(Vuetify, {
+  components: {
+    VApp,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar,
+    transitions
+  }{{#theme}},
+  theme: {
+    primary: '#ee44aa',
+    secondary: '#424242',
+    accent: '#82B1FF',
+    error: '#FF5252',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FFC107'
+  }{{/theme}}
+})
+{{else}}
+Vue.use(Vuetify{{#theme}}, { theme: {
+  primary: '#ee44aa',
+  secondary: '#424242',
+  accent: '#82B1FF',
+  error: '#FF5252',
+  info: '#2196F3',
+  success: '#4CAF50',
+  warning: '#FFC107'
+}}{{/theme}})
+{{/alacarte}}
 
 Vue.config.productionTip = false
 
